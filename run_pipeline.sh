ecd9f2985a19f561e92849a336951e3f24a1382e
Aakash Rao <mrao.aakash@gmail.com>
2023-04-20 15:28:32 +0530

    Added newer path

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -14,17 +14,17 @@ DATASETNAME=LUAD-HistoSeg
 #     --max_epoches 20                        \
 #     >> logs/stage1_log_on_$DATASET.txt
 ##  Generate Pseudo-Mask: Generate 3-level PMs by above model of Stage1.
-python 2_generate_PM.py                       \
-    --dataroot datasets/LUAD-HistoSeg        \
-    --dataset luad                      \
-    --weights checkpoints/stage1_checkpoint_trained_on_$DATASET.pth\
-    >> logs/stagePM_log_on_$DATASET.txt
-# #  Run Stage2: Train the deeplab v3+ model with 3-level pseudo-mask.
-# python 3_train_stage2.py                    \
-#     --dataset luad                      \
+# python 2_generate_PM.py                       \
 #     --dataroot datasets/LUAD-HistoSeg        \
-#     --epochs 30                              \
-#     --Is_GM False                           \
-#     --resume_stage1 checkpoints/stage1_checkpoint_trained_on_$DATASET.pth\
-#     --resume init_weights/deeplab-resnet.pth.tar\
-#     >> logs/stage2_log_on_$DATASET.txt
+#     --dataset luad                      \
+#     --weights checkpoints/stage1_checkpoint_trained_on_$DATASET.pth\
+#     >> logs/stagePM_log_on_$DATASET.txt
+# #  Run Stage2: Train the deeplab v3+ model with 3-level pseudo-mask.
+python 3_train_stage2.py                    \
+    --dataset luad                      \
+    --dataroot datasets/LUAD-HistoSeg        \
+    --epochs 30                              \
+    --Is_GM False                           \
+    --resume_stage1 checkpoints/stage1_checkpoint_trained_on_$DATASET.pth\
+    --resume init_weights/deeplab-resnet.pth.tar\
+    >> logs/stage2_log_on_$DATASET.txt
